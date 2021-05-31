e52b08695a8609919fa855b9fc31b02cea4a9e70
Alex Mokin <mr.aleksay@gmail.com>
2021-05-31 11:15:19 +0500

    nav bar: fixed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -62,7 +62,7 @@
                                 <x-dropdown-link :href="route('logout')"
                                                  onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Log Out') }}
+                                    {{ __('Выход') }}
                                 </x-dropdown-link>
                             </form>
                         </x-slot>
