4acd21ec9f49be8d6c375366b783ac4b3651f406
Alex Mokin <mr.aleksay@gmail.com>
2021-06-01 15:15:28 +0500

    fixed: index.blade and quest.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12 justify-center justify-between">
         <div class="flex max-w-7xl mx-auto sm:p-4 lg:p-8 flex-wrap justify-center">
             @forelse($themes as $theme)
-                <div class="card border bg-red-100 sm:px-6 sm:m-2 lg:px-8 lg:m-4" style="width: 18rem;">
+                <div class="card border bg-red-100 sm:px-6 sm:m-2 lg:px-8 lg:m-4 " style="width: 18rem;">
                     <img src="img/exam.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <div class="items-center inline-flex">
@@ -18,7 +18,7 @@
                             </h5>
                         </div>
                         <p class="card-text font-medium font-semibold text-center">{{ $theme->title }}</p>
-                        <x-btn-link class="btn btn-primary ml-3 sm:m-2 lg:m-6"
+                        <x-btn-link class="btn btn-primary sm:m-2 lg:m-6"
                                     href="{{ route('themes.show', ['id' => $theme->id])}}">
                             {{ __('Пройти тему') }}
                         </x-btn-link>
